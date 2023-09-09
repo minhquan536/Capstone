523c87d91f8b9441d4a2bd1c0276989667beec5f
Trung <trungnguyen.02082000@gmail.com>
2023-09-09 09:34:46 +0700

    reponsive

--- model/sign.js
+++ model/sign.js
@@ -1,11 +1,13 @@
 
 function signUp() {
-    var nam = document.getElementById("#Nam").value;
-    var nu = document.getElementById("#Nu").value;
+    var nam = document.getElementById("Nam");
+    console.log(nam)
+    var nu = document.getElementById("Nu");
     var email = document.querySelector("#taiKhoan").value;
-    var password = document.querySelector("matKhau").value;
+    var password = document.querySelector("#matKhau").value;
     var name = document.querySelector("#hoTen").value;
     var gender = kiemTraGioiTinh(nam, nu);
+
     var phone = document.querySelector("#SDT").value;
     var user = new User(email, password, name, gender, phone);
     console.log(user)
@@ -15,24 +17,24 @@ function signUp() {
         data: user
     });
     promiseObj.then(function (result) {
-        console.log(result);
+        console.log("mnb");
 
     });
     promiseObj.catch(function (error) {
-        //thất bại
         console.log(error);
     })
-    document.querySelector("#btn__home").click();
+
 }
+document.querySelector("#sign__btn").onclick = signUp;
+
 function kiemTraGioiTinh(radio1, radio2) {
     var loai = ""
     if (radio1.checked) {
         loai = "Nam"
     }
-    else if (radio2.checked) {
+    else if (radio2) {
         loai = "Nữ"
     }
-
     else {
         alert("Vui lòng chọn giới tính")
     }
