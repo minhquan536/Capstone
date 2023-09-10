cd1ba132a11ef851da9bee6a4227589cd293417c
Trung <trungnguyen.02082000@gmail.com>
2023-09-10 14:43:30 +0700

    validation form đăng ký

---
+++ model/Validation.js
@@ -0,0 +1,55 @@
+function Validation() {
+    this.checkEmpty = function (value, message, spanID) {
+        if (value.trim() != "") {
+            document.getElementById(spanID).innerHTML = "";
+            document.getElementById(spanID).style.display = "none";
+            return true;
+        }
+        document.getElementById(spanID).innerHTML = message;
+        document.getElementById(spanID).style.display = "block";
+        return false;
+    }
+    this.checkMatKhau = function (value, message, spanID) {
+        var pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{6,10}$/
+        if (value.match(pattern)) {
+            //hợp lệ 
+            document.getElementById(spanID).innerHTML = "";
+            document.getElementById(spanID).style.display = "none";
+            return true;
+        }
+        // không hợp lệ
+        document.getElementById(spanID).innerHTML = message;
+        document.getElementById(spanID).style.display = "block";
+        return false;
+    }
+    this.checkHoTen = function (value, message, spanID) {
+        var pattern = /^[a-z A-Z_ÀÁÂÃÈÉÊẾÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼỀỀỂưăạảấầẩẫậắằẳẵặẹẻẽềềểếỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹý\\s]+$/;
+
+        if (value.match(pattern)) {
+            //hợp lệ
+            document.getElementById(spanID).innerHTML = "";
+            document.getElementById(spanID).style.display = "none";
+            return true;
+        }
+
+        // không hợp lệ
+        document.getElementById(spanID).innerHTML = message;
+        document.getElementById(spanID).style.display = "block";
+        return false;
+
+    }
+    this.checkSDT = function (value, message, spanID) {
+        var pattern = /((09|03|07|08|05)+([0-9]{8})\b)/g;
+        if (value.match(pattern)) {
+            //hợp lệ
+            document.getElementById(spanID).innerHTML = "";
+            document.getElementById(spanID).style.display = "none";
+            return true;
+        }
+
+        // không hợp lệ
+        document.getElementById(spanID).innerHTML = message;
+        document.getElementById(spanID).style.display = "block";
+        return false;
+    }
+}
--- model/sign.js
+++ model/sign.js
@@ -1,42 +1,50 @@
-
+var validation = new Validation();
 function signUp() {
     var nam = document.getElementById("Nam");
-    console.log(nam)
     var nu = document.getElementById("Nu");
-    var email = document.querySelector("#taiKhoan").value;
-    var password = document.querySelector("#matKhau").value;
-    var name = document.querySelector("#hoTen").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("matKhau").value;
+    var name = document.getElementById("hoTen").value;
     var gender = kiemTraGioiTinh(nam, nu);
+    var phone = document.getElementById("SDT").value;
+    var isValid = true;
+    isValid &= validation.checkEmpty(email, "Tài Khoản không được để trống", "spanTKUP") && validation.checkTK(taiKhoan, "Tài khoản viên không hợp lệ ", "spanTKUP");
+    isValid &= validation.checkEmpty(password, "Mật khẩu không được để trống", "spanMKUP") && validation.checkMatKhau(password, "Mật khẩu từ 6-10 ký tự chứa ít nhất 1 ký tự số, 1 ký tự in hoa", "spanMKUP");
+    isValid &= validation.checkEmpty(name, "Họ tên không được để trống", "spanTenUP") && validation.checkHoTen(name, "Họ tên không hợp lệ", "spanTenUP");
+    isValid &= validation.checkEmpty(phone, "Số điện thoại không được để trống", "spanSDTUP") && validation.checkSDT(phone, "Số điện thoại không hợp lệ ", "spanSDTUP");
+    if (isValid) {
+        var user = new User(email, password, name, gender, phone);
+        console.log(user)
+        var promiseObj = axios({
+            method: 'post',
+            url: 'https://shop.cyberlearn.vn/api/Users/signup',
+            data: user
+        });
+        promiseObj.then(function (result) {
+            console.log(result);
+            alert(result.data.message)
+            // if (result) { window.location.href = "../view/dangnhap.html"; }
 
-    var phone = document.querySelector("#SDT").value;
-    var user = new User(email, password, name, gender, phone);
-    console.log(user)
-    var promiseObj = axios({
-        method: 'post',
-        url: 'https://shop.cyberlearn.vn/api/Users/signup',
-        data: user
-    });
-    promiseObj.then(function (result) {
-        console.log("mnb");
+        }).catch(function (error) {
+            console.log(error);
+            alert(error.response.data.message)
+        });
 
-    });
-    promiseObj.catch(function (error) {
-        console.log(error);
-    })
+    }
 
 }
-document.querySelector("#sign__btn").onclick = signUp;
-
+document.getElementById("btn__sign").onclick = signUp;
 function kiemTraGioiTinh(radio1, radio2) {
-    var loai = ""
+    var gioiTinh = null
     if (radio1.checked) {
-        loai = "Nam"
-    }
-    else if (radio2) {
-        loai = "Nữ"
+        gioiTinh = true
+    } else if (radio2.checked) {
+        gioiTinh = false
     }
     else {
-        alert("Vui lòng chọn giới tính")
+        alert("Chọn giới tính")
     }
-    return loai;
+    return gioiTinh
 }
+
+
